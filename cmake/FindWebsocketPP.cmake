5c5de92bfbad94610e388812bb7b767720e6d507
Jeff Mitchell <jeff@tomahawk-player.org>
2013-01-17 16:10:58 -0500

    Modify to build with no help from internal TH build

--- cmake/FindWebsocketPP.cmake
+++
@@ -1,5 +0,0 @@
-set( WEBSOCKETPP_INCLUDE_DIR ${CMAKE_BINARY_DIR}/thirdparty/websocketpp/src/include )
-set( WEBSOCKETPP_LIBRARY tomahawk_websocketpp )
-set( WEBSOCKETPP_LIBRARIES ${WEBSOCKETPP_LIBRARY} )
-
-add_subdirectory( ${CMAKE_CURRENT_LIST_DIR}/../thirdparty/websocketpp/src )
