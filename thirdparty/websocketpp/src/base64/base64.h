6de42ba4bec2c54d921ecd7009d2ff686cbc03c9
Jeff Mitchell <jeff@tomahawk-player.org>
2013-05-07 12:33:52 -0400

    Remove websocketpp

--- thirdparty/websocketpp/src/base64/base64.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-#include "../dllexport.h"
-
-_DLLEXPORT std::string base64_encode(unsigned char const* , unsigned int len);
-_DLLEXPORT std::string base64_decode(std::string const& s);
